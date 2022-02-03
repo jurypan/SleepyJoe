c79a9b4dca782801d20ba96733a8879be9b225b0
jurypan <info@jpx.email>
2022-02-03 12:49:55 +0100

    Setup basics of the token

--- src/contracts/Token.sol
+++ src/contracts/Token.sol
@@ -1,8 +1,15 @@
 // SPDX-License-Identifier: MIT
 
-pragma solidity ^0.8.11;
+pragma solidity ^0.5.16;
 
 contract Token {
-    string name = "JPX";
+    string public name = "Sleepy Joe";
+    string public symbol = "JPX";
+    uint256 public decimals = 18;
+    uint256 public totalSupply;
+
+    constructor() public {
+        totalSupply = 1000000 * (10 ** decimals);
+    }
 }
 
